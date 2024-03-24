770a966ec62351029508616c6bf2ccc0e9a9ffcb
alieniig <s_lienig22@hwr-berlin.de>
2024-03-24 15:43:00 +0100

    aufgabe 1

--- TestOnlineSQLDB.java
+++ TestOnlineSQLDB.java
@@ -16,12 +16,20 @@ public class TestOnlineSQLDB {
             Statement stmt = connection.createStatement();
 
             // Fahrzeug 2
-            stmt.executeUpdate("INSERT INTO Fahrzeuge VALUES ('Audi', 'Q7', 200)");
-
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Fahrzeuge");
-            while (rs.next()) {
+            //stmt.executeUpdate("INSERT INTO Kunden VALUES ('André', 'Lienig', '2004-07-07')");
+            //Ausgabe Fahrzeugtabelle
+            ResultSet rs1 = stmt.executeQuery("SELECT * FROM Fahrzeuge");
+            System.out.println("Ausgabe Fahrzeuge");
+            while (rs1.next()) {
                 System.out.println(
-                        rs.getString("Hersteller") + " " + rs.getString("Modell") + " " + rs.getInt("Preis pro Tag"));
+                        rs1.getString("Hersteller") + " " + rs1.getString("Modell") + " " + rs1.getInt("Preis pro Tag"));
+            }
+            //Ausgabe Fahrzeugtabelle
+            ResultSet rs2 = stmt.executeQuery("SELECT * FROM Kunden");
+            System.out.println("Ausgabe Kunden");
+            while (rs2.next()) {
+                System.out.println(
+                        rs2.getString("Vorname") + " " + rs2.getString("Nachname") + " " + rs2.getDate("Geburtsdatum"));
             }
 
             connection.close();
