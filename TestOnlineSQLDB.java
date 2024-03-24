6d8c5b6739f690f2c34968c74e1a071615c03a1a
alieniig <s_lienig22@hwr-berlin.de>
2024-03-24 16:18:08 +0100

    methoden result set

--- TestOnlineSQLDB.java
+++ TestOnlineSQLDB.java
@@ -12,35 +12,14 @@ public class TestOnlineSQLDB {
             String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11693896";
             String user = "sql11693896";
             String password = "bdIfjnDgeA";
-            connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password); // Instanz des JDBC-Treibers
             Statement stmt = connection.createStatement();
 
-            //Kunden einfügen - INSERT INTO
+            // Kunden einfügen - INSERT INTO
             stmt.executeUpdate("INSERT INTO Kunden VALUES ('Max', 'Mustermann', '1980-01-01')");
             stmt.executeUpdate("INSERT INTO Kunden VALUES ('Erika', 'Musterfrau', '1985-02-02')");
             stmt.executeUpdate("INSERT INTO Kunden VALUES ('John', 'Doe', '1990-03-03')");
             stmt.executeUpdate("INSERT INTO Kunden VALUES ('Jane', 'Doe', '1995-04-04')");
-
-            // Ausgabe Fahrzeugtabelle - SELECT
-            ResultSet rs1 = stmt.executeQuery("SELECT * FROM Fahrzeuge");
-            System.out.println("Ausgabe Fahrzeuge");
-            while (rs1.next()) {
-                System.out.println(
-                        rs1.getString("Hersteller") + " " + rs1.getString("Modell") + " "
-                                + rs1.getInt("Preis pro Tag"));
-            }
-            // Ausgabe Fahrzeugtabelle - SELECT
-            ResultSet rs2 = stmt.executeQuery("SELECT * FROM Kunden");
-            System.out.println("Ausgabe Kunden");
-            while (rs2.next()) {
-                System.out.println(
-                        rs2.getString(1) + " " + rs2.getString(2) + " " + rs2.getDate(3));
-            }
-            //Kunden löschen - DELETE
-            stmt.executeUpdate("DELETE FROM Kunden WHERE Vorname = 'Max'");
-
-            //Fahrzeug ändern - UPDATE
-            stmt.executeUpdate("UPDATE Fahrzeuge SET `Preis pro Tag` = 100 WHERE Hersteller = 'BMW' AND Modell = 'X5'");
 
             // Ausgabe Fahrzeugtabelle - SELECT
             ResultSet rs3 = stmt.executeQuery("SELECT * FROM Fahrzeuge");
@@ -49,8 +28,16 @@ public class TestOnlineSQLDB {
                 System.out.println(
                         rs3.getString("Hersteller") + " " + rs3.getString("Modell") + " "
                                 + rs3.getInt("Preis pro Tag"));
+                System.out.println("Row number: " + rs3.getRow());
+                System.out.println("Column count: " + rs3.getMetaData().getColumnCount());
+                System.out.println("Column name of index 1: " + rs3.getMetaData().getColumnName(1));
+                System.out.println("Column type of index 1: " + rs3.getMetaData().getColumnType(1));
+                System.out.println("Is the cursor on the last row? " + rs3.isLast());
+                System.out.println("Is the cursor on the first row? " + rs3.isFirst());
+                System.out.println("Is the cursor on a valid row? " + rs3.isBeforeFirst());
+                System.out.println("Is the cursor on a valid row? " + rs3.isAfterLast());
+
             }
-            // Ausgabe Fahrzeugtabelle - SELECT
             ResultSet rs4 = stmt.executeQuery("SELECT * FROM Kunden");
             System.out.println("Ausgabe Kunden");
             while (rs4.next()) {
